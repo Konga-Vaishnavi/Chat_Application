2c6617c6d655f8e466578073edb5d8a1303d35b2
Mamatha-1906 <domalamamatha06@gmail.com>
2025-11-29 21:53:55 +0530

    logincss

--- simplejs.js
+++ simplejs.js
@@ -3,3 +3,9 @@ console.log("Iam Vaishnavi")
 console.log("CSM")
 console.log("from vaagdevi college")
 console.log("B.Tech")
+<<<<<<< Updated upstream
+=======
+console.log("nexgen problem")
+
+
+>>>>>>> Stashed changes
